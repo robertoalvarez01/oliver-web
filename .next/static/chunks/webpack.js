88a96f7b42e70bfd42103819e121f31b23906b07
Franco Benitez <franco_casla_benitez@hotmail.com>
2021-03-15 18:55:40 -0300

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "223d5a7f70a79f0dc34d";
+/******/ 	var hotCurrentHash = "a9c84923ed19ffae03fe";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
